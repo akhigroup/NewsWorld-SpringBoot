edf0299f1663cc43ec05c5ac03a7b1f5ebc9831b
Rafal <rkpieniazek@gmail.com>
2016-02-29 14:17:09 +0100

    mailService

---
+++ src/main/java/pl/pwr/news/service/MailService.java
@@ -0,0 +1,9 @@
+package pl.pwr.news.service;
+
+/**
+ * Created by Rafal on 2016-02-29.
+ */
+public interface MailService {
+
+    void sendSimpleMessage();
+}
--- src/main/java/pl/pwr/news/webapp/HomeController.java
+++ src/main/java/pl/pwr/news/webapp/HomeController.java
@@ -1,9 +1,11 @@
 package pl.pwr.news.webapp;
 
 import lombok.extern.log4j.Log4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.pwr.news.service.MailService;
 
 /**
  * Created by Evelan-E6540 on 27.02.2016.
@@ -12,8 +14,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Log4j
 public class HomeController {
 
+    @Autowired
+    MailService mailService;
+
+
     @RequestMapping(value = "/")
     public String home(Model model) {
+        mailService.sendSimpleMessage();
+
         return "index";
     }
 }
