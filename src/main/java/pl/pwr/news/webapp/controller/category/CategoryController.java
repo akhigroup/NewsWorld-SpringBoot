45e840c1bba70dc903de5dfd23aead58f65eab80
evelan <evelan3094@gmail.com>
2016-04-02 21:19:31 +0200

    category fix

--- src/main/java/pl/pwr/news/webapp/controller/category/CategoryController.java
+++ src/main/java/pl/pwr/news/webapp/controller/category/CategoryController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import pl.pwr.news.model.article.Article;
 import pl.pwr.news.model.category.Category;
 import pl.pwr.news.model.tag.Tag;
 import pl.pwr.news.service.article.ArticleService;
