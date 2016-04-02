810e432bac43234a301ee9ae8ab0d191e3a0da05
evelan <evelan3094@gmail.com>
2016-04-02 16:05:39 +0200

    edit/create article

--- src/main/java/pl/pwr/news/webapp/controller/article/ArticleController.java
+++ src/main/java/pl/pwr/news/webapp/controller/article/ArticleController.java
@@ -1,20 +1,24 @@
 package pl.pwr.news.webapp.controller.article;
 
+import com.google.common.base.Splitter;
 import lombok.extern.log4j.Log4j;
+import org.elasticsearch.common.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import pl.pwr.news.model.article.Article;
 import pl.pwr.news.model.category.Category;
 import pl.pwr.news.model.tag.Tag;
 import pl.pwr.news.service.article.ArticleService;
 import pl.pwr.news.service.category.CategoryService;
 import pl.pwr.news.service.tag.TagService;
+import pl.pwr.news.webapp.controller.article.form.AddArticleForm;
 
 import java.util.List;
+import java.util.Set;
+
+import static org.elasticsearch.common.lang3.StringUtils.isNotBlank;
 
 /**
  * Created by jakub on 3/22/16.
@@ -47,16 +51,50 @@ public class ArticleController {
 
         model.addAttribute("tagList", tagList);
         model.addAttribute("categoryList", categoryList);
-        model.addAttribute("newArticle", new Article());
-        return "article/add";
+        model.addAttribute("addArticleForm", new AddArticleForm());
+        return "article/add-edit";
     }
 
-    @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public String saveNewArticle(@ModelAttribute(value = "newArticle") Article article) {
+    @RequestMapping(value = "/edit", method = RequestMethod.GET)
+    public String editArticle(@RequestParam("id") Long articleId, Model model) {
+        Article article = articleService.findById(articleId);
+        List<Tag> allTags = tagService.findAll();
+        List<Category> categoryList = categoryService.findAll();
+        AddArticleForm addArticleForm = new AddArticleForm(article);
 
-        articleService.createOrUpdate(article);
+        allTags.removeAll(article.getTags());
 
-        return "redirect:/admin/article/add";
+        model.addAttribute("checkedTags", article.getTags());
+        model.addAttribute("tagList", allTags);
+        model.addAttribute("categoryList", categoryList);
+        model.addAttribute("addArticleForm", addArticleForm);
+        return "article/add-edit";
     }
 
+    @RequestMapping(value = "/process", method = RequestMethod.POST)
+    public String createOrUpdateArticle(@ModelAttribute(value = "addArticleForm") AddArticleForm addArticleForm) {
+        Article articleToProcess = addArticleForm.getArticle();
+        articleToProcess = articleService.createOrUpdate(articleToProcess);
+        Long articleId = articleToProcess.getId();
+        String tagsInString = addArticleForm.getTagsInString();
+
+        if (isNotBlank(tagsInString)) {
+            Iterable<String> checkedTags = Splitter.on(',')
+                    .trimResults()
+                    .omitEmptyStrings()
+                    .split(tagsInString);
+
+            Set<Tag> articleTags = articleToProcess.getTags();
+            for (Tag tag : articleTags) {
+                Long tagId = tag.getId();
+                articleService.removeTag(articleId, tagId);
+            }
+
+            for (String tag : checkedTags) {
+                Tag createdTag = tagService.createTag(new Tag(tag));
+                articleService.addTag(articleId, createdTag.getId());
+            }
+        }
+        return "redirect:/admin/article/list";
+    }
 }
