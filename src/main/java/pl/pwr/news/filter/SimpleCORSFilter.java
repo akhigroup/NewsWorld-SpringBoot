59f2558364596c8b8f9b39aba3485c2f3f56f747
Jasiek <janekmoroz@gmail.com>
2016-04-10 12:42:09 +0200

    może teraz /oauth/token zadziała

--- src/main/java/pl/pwr/news/filter/SimpleCORSFilter.java
+++ src/main/java/pl/pwr/news/filter/SimpleCORSFilter.java
@@ -3,6 +3,7 @@ package pl.pwr.news.filter;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -14,11 +15,18 @@ public class SimpleCORSFilter  implements Filter {
 
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
+
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Key");
-        chain.doFilter(req, res);
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, res);
+        }
     }
 
     public void init(FilterConfig filterConfig) {
