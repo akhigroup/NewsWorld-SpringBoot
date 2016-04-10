5f41a75587f9b42af6d693454b7fcc36d040140d
Jasiek <janekmoroz@gmail.com>
2016-04-10 21:24:14 +0200

    Jenkins, będź grzeczny

--- src/main/java/pl/pwr/news/config/SecurityConfiguration.java
+++ src/main/java/pl/pwr/news/config/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -35,6 +36,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         http
                 .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/oauth/token").permitAll()
                 .antMatchers("/admin/**").authenticated()
                 .anyRequest().permitAll()
                 .and()
