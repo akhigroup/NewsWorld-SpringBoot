55244baee2d5746e98f1af05c6d043e8aaf5d4dd
rkpieniazek <rkpieniazek@gmail.com>
2016-05-30 09:49:03 +0200

    Java docs added

--- src/main/java/pl/pwr/news/webapp/controller/user/UserApi.java
+++ src/main/java/pl/pwr/news/webapp/controller/user/UserApi.java
@@ -17,7 +17,10 @@ import pl.pwr.news.webapp.controller.user.dto.UserDTO;
 import java.util.List;
 
 /**
- * Created by Evelan on 23/04/16.
+ * Provides REST Api methods for @{@link User} account managment
+ * @author Jakub Pomykała, Jacek Falfasiński, Jan Moroz, Rafał Pieniążek
+ * @version 1.0
+ *
  */
 @RestController
 @RequestMapping("/api")
@@ -26,7 +29,18 @@ public class UserApi {
 
     @Autowired
     UserService userService;
-
+    /**
+     * Method for registratin user.
+     * It consumes data provided in JSON format. It takes:
+     * @param email
+     * @param password
+     * @param firstName
+     * @param lastName
+     *
+     * This method returns registered User with status 200(success), or 405 HTTP message(conflict), if email is not unique.
+     * Data returned are strictly connected with {@link UserDTO}
+     * @return UserDTO
+     */
     @RequestMapping(value = "/user/register", method = RequestMethod.GET)
     public Response<UserDTO> register(@RequestParam String email,
                                       @RequestParam String password,
@@ -46,7 +60,16 @@ public class UserApi {
         return new Response<>(userDTO);
     }
 
-
+    /**
+     *
+     * @param email
+     * @param password
+     * @return UserDTO with credientals found in database.
+     * If email does not exist in database an 401HTTP code(Unauthorized)  is returned
+     * If email provided exist in databse, but password is incorect 401HTTP code is returned, with
+     *"Bad Credentials" message
+     * @see RegisterController for admin loging.
+     */
     @RequestMapping(value = "/user/login", method = RequestMethod.GET)
     public Response<UserDTO> login(@RequestParam String email,
                                    @RequestParam String password) {
@@ -68,6 +91,14 @@ public class UserApi {
         return new Response<>(userDTO);
     }
 
+    /**
+     * This method allows to save articles that user clicked to be his favourite.
+     * It takes two params, in JSON format:
+     * @param articleId
+     * @param token - obtained from {@link #login(String,String)}.
+     * @return UserDTO with User, or 401HTTP code if any User with provided token is found.
+     *
+     */
     @RequestMapping(value = "/user/favourite", method = RequestMethod.POST)
     public Response<UserDTO> addFavourite(@RequestParam Long articleId,
                                           @RequestParam String token) {
@@ -86,6 +117,14 @@ public class UserApi {
         return new Response<>("000", "OK");
     }
 
+    /**
+     * This method allows to obtain all articles which was clicked 'favourite' bu User.
+     * It takes token as a param, in JSON format. Method will search User data with token provided
+     *
+     * @param token - obtained from {@link #login(String,String)}.
+     * @return List of {@link ArticleDTO}
+     *
+     */
     @RequestMapping(value = "/user/favourite", method = RequestMethod.GET)
     public Response<List<ArticleDTO>> getFavourites(@RequestParam String token) {
 
@@ -103,6 +142,15 @@ public class UserApi {
         return new Response<>(articleDTOList);
     }
 
+    /**
+     * This method allows to increment tag's weigh, when user finds article interesting.
+     * It takes two params, provided in JSON format.
+     * @param tagId
+     * @param token
+     * Method will search User data with token provided and then add proper wage in connection beteen tables in databse.
+     * @return value of actual tag weight for User, after inrementation.
+     * It would return 401 HTTP message, when token not exist in database.
+     */
     @RequestMapping(value = "/user/tag", method = RequestMethod.POST)
     public Response<Long> incrementTagValue(@RequestParam Long tagId, @RequestParam String token) {
         try {
@@ -112,7 +160,15 @@ public class UserApi {
             return new Response<>("-1", ex.getMessage());
         }
     }
-
+    /**
+     * This method allows to obtain tag's weigh
+     * It takes two params, provided in JSON format.
+     * @param tagId
+     * @param token
+     * Method will search User data with token provided and then return wage for {@link pl.pwr.news.model.tag.Tag}.
+     * @return value of actual tag weight for User found by Token
+     * It would return 401 HTTP message, when token not exist in database.
+     */
     @RequestMapping(value = "/user/tag", method = RequestMethod.GET)
     public Response<Long> getTagValue(@RequestParam Long tagId, @RequestParam String token) {
         try {
